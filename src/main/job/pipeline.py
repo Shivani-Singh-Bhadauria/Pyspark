20e12e66ae0e6c473c64bd2a236cb7693e2d8a59
Shivani-Singh-Bhadauria <83387584+Shivani-Singh-Bhadauria@users.noreply.github.com>
2024-05-27 12:51:31 +0530

    Update pipeline.py

--- src/main/job/pipeline.py
+++ src/main/job/pipeline.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession, DataFrame, Window
+from pyspark.sql.functions import *
 from main.base import PySparkJobInterface
 import pyspark.sql.functions as F
 
@@ -6,21 +7,25 @@ import pyspark.sql.functions as F
 class PySparkJob(PySparkJobInterface):
 
     def init_spark_session(self) -> SparkSession:
-        # TODO: put your code here
-        ...
+        spark = SparkSession.builder.appName('Data Cleaning').getOrCreate()
+        return spark
+    
 
     def filter_medical(self, eligibility: DataFrame, medicals: DataFrame) -> DataFrame:
-        # TODO: put your code here
-        ...
+        return medicals.join(eligibility,eligibility.memberId == medicals.memberId,"left_semi")
 
     def generate_full_name(self, eligibility: DataFrame, medical: DataFrame) -> DataFrame:
-        # TODO: put your code here
-        ...
+        medicals = medical.join(eligibility,['memberId'],"left")\
+                .withColumn('fullName',concat(eligibility.firstName,lit(' '),eligibility.lastName))\
+                .select('memberId','fullName','paidAmount')
+        return medicals
 
     def find_max_paid_member(self, medicals: DataFrame) -> str:
-        # TODO: put your code here
-        ...
+        memberid = medicals.sort("paidAmount", ascending=False).first().memberId
+        return str(memberid)
 
     def find_total_paid_amount(self, medicals: DataFrame) -> int:
-        # TODO: put your code here
-        ...
+        total_amt = medicals.agg({'paidAmount':'sum'})\
+                .withColumnRenamed('sum(paidAmount)','total_amt')\
+                .first().total_amt
+        return total_amt
